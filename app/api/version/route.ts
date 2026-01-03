8ecf0a9fcd17af9fbf414ae3e2ed3733d53e4cb7
Rahul Patni <rahul20patni@gmail.com>
2026-01-02 19:19:01 -0800

    new min version

--- app/api/version/route.ts
+++ app/api/version/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 // Minimum required app version - update this when you have breaking changes
-const MIN_VERSION = '1.0.8';
+const MIN_VERSION = '1.0.9';
 
 // TestFlight public link for your app
 const TESTFLIGHT_URL = 'https://testflight.apple.com/join/6u5zHFms';
